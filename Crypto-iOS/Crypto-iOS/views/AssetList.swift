da95a0ef6f0ef588fe47f445550f9cfe04b11d91
wtoledo2012 <walthertoledo@gmail.com>
2025-04-12 13:51:53 -0500

    clase 2 app ios-1

--- Crypto-iOS/Crypto-iOS/views/AssetList.swift
+++ Crypto-iOS/Crypto-iOS/views/AssetList.swift
@@ -5,21 +5,30 @@ struct AssetList: View {
     var viewModel: AssetListViewModel = .init()
     
     var body: some View {
-        Text(viewModel.errorMessage ?? "")
-        List {
-            ForEach(viewModel.assets) { asset in
-                AssetView(assetViewState: .init(asset))
+        NavigationStack{
+            Text(viewModel.errorMessage ?? "")
+            List {
+                ForEach(viewModel.assets) { asset in
+                    NavigationLink{
+                        AssetDetailView(asset:asset)
+                    } label: {
+                        AssetView(assetViewState: .init(asset))
+                    }
+                    
+                }
             }
-        }
-        .listStyle(.plain)
-        .onAppear{
-            
-        }
-        .onDisappear{
-            
-        }
-        .task {
-            await viewModel.fetchAssets()
+            .listStyle(.plain)
+            /*.onAppear{
+                
+            }
+            .onDisappear{
+                
+            }*/
+            .listStyle(.plain)
+            .task {
+                await viewModel.fetchAssets()
+            }
+            .navigationTitle("Home")
         }
     }
 }
